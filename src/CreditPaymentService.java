84ec7dd421a5d3f366fafe9b33d58063737d9c4c
Ananiev_IM <ilya.anunev@gmail.com>
2024-10-06 14:35:34 +0700

    'add new variable percentMonth instead of manipulations with percentYear'

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,6 @@
 public class CreditPaymentService {
     public double calculate(double sumRub, double periodMonth, double percentYear) {
-        return sumRub * (percentYear/100/12) * Math.pow((1 + (percentYear/100/12)), periodMonth) / (Math.pow((1 + (percentYear/100/12)), periodMonth) - 1);
+        double percentMonth = percentYear / 100 / 12;
+        return sumRub * percentMonth * Math.pow((1 + percentMonth), periodMonth) / (Math.pow((1 + percentMonth), periodMonth) - 1);
     }
 }
